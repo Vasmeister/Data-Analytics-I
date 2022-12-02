64f9d91717831d80723dd5e6a9f4463c7f1104be
elenatrevisani <118910166+elenatrevisani@users.noreply.github.com>
2022-12-02 16:22:05 +0100

    Update G10-T3-deMattamouros_Odermatt_Trevisani_Zhuravlev copy.R

--- G10-T3-deMattamouros_Odermatt_Trevisani_Zhuravlev copy.R
+++ G10-T3-deMattamouros_Odermatt_Trevisani_Zhuravlev copy.R
@@ -195,3 +195,185 @@ ggplot() +
   ylab("MSE")+ 
   annotate("text", x=4, y=12, label= "In blue: MSEs out sample") +
   annotate("text", x=4, y=11, label= "In orange: MSEs in sample")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################################          RMARKDOWN DRAFT       ######################################
+
+
+
+
+
+
+
+---
+title: "PS2_Markdown"
+author: 
+- José Maria de Mattamouros Resende Fonseca de Oliveira (22-602-783)
+- Linda Fiorina Odermatt (17-946-310)
+- Elena Trevisani (22-620-603) 
+- Vasily Zhuravlev (18-502-401) 
+date: "2022-12-02"
+output: pdf_document
+---
+
+## Exercise 1
+
+```{r, results=FALSE}
+load("/Users/nenetrevisani/Desktop/data an/DA2/student-mat-test.Rdata")
+load("/Users/nenetrevisani/Desktop/data an/DA2/student-mat-train.Rdata")
+```
+```{r}
+nrow(data)
+ncol(data)
+```
+
+There are 143 observations in the test set, and 214 observations in the train set.
+
+```{r}
+nrow(test)/(nrow(test)+nrow(train)) 
+```
+
+The ratio is roughly 40:60% between the test and the train data sets.
+The sizes of the two data sets are within the recommended sizes 20-40% for test set and 60-80% for the training set.
+
+## Exercise 2
+
+```{r}
+grade_summary=matrix(data=data.frame(col1=mean(train$G3),
+                                     col2=min(train$G3),
+                                     col3=max(train$G3)))
+colnames(grade_summary)=c("G3")
+rownames(grade_summary)=c("Mean","Min","Max")
+
+grade_summary
+```
+
+Therefore, we find the following in the training data:the average grade is 11.64, the minimum is 4 and the maximum is 19.00.
+
+## Exercise 3
+```{r, results=FALSE, include=FALSE}
+library(ggplot2)
+```
+```{r}
+ggplot(data=train, aes(x=train$G3)) + 
+  geom_histogram(binwidth =1 , color = "darkblue", fill = "lightblue") +
+  ggtitle("Histogram for Student Grades in Maths, rounded to the closest figure")+
+  ylab("Count")+
+  xlab("Final Math Grade (0-20)")
+```
+
+Notice the data is fairly normally distributed, there are no large outliers for which we need to tranfor the data.
+
+## Exercise 4
+
+add text
+
+## Exercise 5
+
+Chosen covariates and why:absences: presence durgin classes has been shown to positively impact a student's engagement with the subject studied.schoolsup: extra educational school support indeed may affect the knowledge and engagement of a student with his education. studytime: hours spent although may vary depending on background of esch individual may highly impact the final grade.
+higher: a motivated person may very likely achieve higher grades as math is a core subject in many advanced higher education. levels. internet: access to it means access to information.
+
+
+```{r}
+lm.5vars <- lm(data = train, G3 ~ absences + internet +  schoolsup + higher + studytime)
+summary(lm.5vars)
+
+lm.fo.5.interact <- lm(data = train, G3 ~ absences + internet +  schoolsup + higher + studytime + (absences + internet +  schoolsup + higher + studytime )^2)
+summary(lm.fo.5.interact)
+
+MSE.5vars.in.sample <- mean((train$G3 - lm.5vars$fitted.values)^2)
+print(paste("MSE Model With 5 Variables:", round(MSE.5vars.in.sample, 4))) 
+
+MSE.fo.5.in.sample <- mean((train$G3 - lm.fo.5.interact$fitted.values)^2)
+print(paste("MSE Model With Interaction Variables:", round(MSE.fo.5.in.sample, 4))) 
+```
+
+Apparently the MSE is lower with more covariates. This is due to the fact that we train our model in-sample and we test it in-sample. The model logically will do a better job at predicting the test score the more covariates we add, as each will add a little more information to what is in the error term.
+
+## Exercise 6
+
+Variables chosen: Fedu, failures, famsup, paid, freetime. Bc ...
+
+```{r}
+lm.10vars <- lm(data = train, G3 ~ absences + internet +  schoolsup + 
+                  higher + studytime + Fedu + failures + famsup + 
+                  paid + freetime)
+summary(lm.10vars)
+
+lm.fo.10.interact <- lm(data = train, G3 ~ absences + internet +  schoolsup + 
+                          higher + studytime + Fedu + failures + famsup + 
+                          paid + freetime + (absences + internet +  schoolsup + 
+                                               higher + studytime + Fedu + failures + famsup + 
+                                               paid + freetime)^2)
+summary(lm.fo.10.interact)
+
+MSE.10vars.in.sample <- mean((train$G3 - lm.10vars$fitted.values)^2)
+print(paste("MSE Model With 10 Variables:", round(MSE.10vars.in.sample, 4))) 
+
+MSE.fo.10.in.sample <- mean((train$G3 - lm.fo.10.interact$fitted.values)^2)
+print(paste("MSE Model With 10 and Interaction Variables:", round(MSE.fo.10.in.sample, 4)))
+
+# We specify new data
+fitted.1 <- predict(lm.5vars, newdata = test) # We obtain a new object in the environment with exactly 143 entries.
+
+MSE.5vars.out.sample <- mean((test$G3 - fitted.1)^2)
+print(paste("MSE Model With 5 Variables:", round(MSE.5vars.out.sample, 4))) 
+
+fitted.2 <- predict(lm.fo.5.interact, newdata = test)
+
+MSE.fo.5.out.sample <- mean((test$G3 - fitted.2)^2)
+print(paste("MSE Model 5 Variables With Interaction Variables:", round(MSE.fo.5.out.sample, 4))) 
+
+fitted.3 <- predict(lm.10vars, newdata = test)
+
+MSE.10vars.out.sample <- mean((test$G3 - fitted.3)^2)
+print(paste("MSE Model With 10 Variables:", round(MSE.10vars.out.sample, 4))) 
+
+fitted.4 <- predict(lm.fo.10.interact, newdata = test)
+
+MSE.fo.10.out.sample <- mean((test$G3 - fitted.4)^2)
+print(paste("MSE Model 10 Variables With Interaction Variables:", round(MSE.fo.10.out.sample, 4))) 
+```
+
+Apparently, the functional form with 10 variables fits the best (model 3).
+
+#```{r}
+MSEout <- data.frame(model = c("Model.5.vars", "Model.5.inter", "Model.10.vars", "Model.10.inter"), MSE.out = c(MSE.5vars.out.sample, MSE.fo.5.out.sample , MSE.10vars.out.sample , MSE.fo.10.out.sample))
+MSEin <- data.frame(model = c("Model.5.vars", "Model.5.inter", "Model.10.vars", "Model.10.inter"), MSE.in = c(MSE.5vars.in.sample, MSE.fo.5.in.sample, MSE.10vars.in.sample, MSE.fo.10.in.sample))
+
+combined.data<- cbind(MSEin, MSEout)
+
+ggplot() +
+  geom_col(data = MSEout, aes(model, MSE.out), fill="blue", alpha = 0.2) +
+  geom_col(data = MSEin, aes(model, MSE.in), fill="orange2", alpha = 0.6) +
+  ylab("MSE")+ 
+  annotate("text", x=4, y=12, label= "In blue: MSEs out sample") +
+  annotate("text", x=4, y=11, label= "In orange: MSEs in sample")
+Footer
+#```
+
+
+
